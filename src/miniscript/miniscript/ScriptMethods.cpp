98e8b5e2271af32a364e3ecde03a26eb697e07b9
Andreas Drewke <andreas@drewke.net>
2023-11-28 17:34:10 +0100

    Context: some initial WIP (4)

--- src/miniscript/miniscript/ScriptMethods.cpp
+++ src/miniscript/miniscript/ScriptMethods.cpp
@@ -1,5 +1,7 @@
 #include <span>
 
+#include <memory>
+
 #include <miniscript/miniscript.h>
 #include <miniscript/miniscript/MiniScript.h>
 #include <miniscript/miniscript/ScriptMethods.h>
@@ -7,6 +9,7 @@
 #include <miniscript/utilities/Time.h>
 
 using std::span;
+using std::make_unique;
 
 using miniscript::miniscript::ScriptMethods;
 
@@ -402,6 +405,88 @@ void ScriptMethods::registerMethods(MiniScript* miniScript) {
 				}
 			};
 			miniScript->registerMethod(new ScriptMethodContextScriptCall(miniScript));
+		}
+		{
+			//
+			class ScriptMethodContextScriptLoadScript: public MiniScript::ScriptMethod {
+			private:
+				MiniScript* miniScript { nullptr };
+			public:
+				ScriptMethodContextScriptLoadScript(MiniScript* miniScript):
+					MiniScript::ScriptMethod(
+						{
+							{ .type = MiniScript::TYPE_STRING, .name = "scriptId", .optional = false, .reference = false, .nullable = false },
+							{ .type = MiniScript::TYPE_STRING, .name = "pathName", .optional = false, .reference = false, .nullable = false },
+							{ .type = MiniScript::TYPE_STRING, .name = "fileName", .optional = false, .reference = false, .nullable = false },
+							{ .type = MiniScript::TYPE_BOOLEAN, .name = "verbose", .optional = true, .reference = false, .nullable = false },
+						},
+						MiniScript::TYPE_NULL
+					),
+					miniScript(miniScript) {}
+				const string getMethodName() override {
+					return "context.script.loadScript";
+				}
+				void executeMethod(span<MiniScript::ScriptVariable>& argumentValues, MiniScript::ScriptVariable& returnValue, const MiniScript::ScriptStatement& statement) override {
+					string scriptId;
+					string pathName;
+					string fileName;
+					bool verbose = false;
+					if (MiniScript::getStringValue(argumentValues, 0, scriptId) == false ||
+						MiniScript::getStringValue(argumentValues, 1, pathName) == false ||
+						MiniScript::getStringValue(argumentValues, 2, fileName) == false ||
+						MiniScript::getBooleanValue(argumentValues, 3, verbose, true) == false) {
+						Console::println(getMethodName() + "(): " + miniScript->getStatementInformation(statement) + ": argument mismatch: expected arguments: " + miniScript->getArgumentInformation(getMethodName()));
+						miniScript->startErrorScript();
+					} else {
+						auto script = make_unique<MiniScript>();
+						script->setContext(miniScript->getContext());
+						script->parseScript(
+							pathName,
+							fileName
+						);
+						if (script != nullptr) {
+							// verbose
+							if (verbose == true) Console::println(script->getInformation());
+							//
+							if (script->isValid() == false) {
+								Console::println("Script not valid.");
+							} else {
+								miniScript->getContext()->addScript(scriptId, script.release());
+							}
+						}
+					}
+				}
+			};
+			miniScript->registerMethod(new ScriptMethodContextScriptLoadScript(miniScript));
+		}
+		{
+			//
+			class ScriptMethodContextScriptRemoveScript: public MiniScript::ScriptMethod {
+			private:
+				MiniScript* miniScript { nullptr };
+			public:
+				ScriptMethodContextScriptRemoveScript(MiniScript* miniScript):
+					MiniScript::ScriptMethod(
+						{
+							{ .type = MiniScript::TYPE_STRING, .name = "scriptId", .optional = false, .reference = false, .nullable = false }
+						},
+						MiniScript::TYPE_NULL
+					),
+					miniScript(miniScript) {}
+				const string getMethodName() override {
+					return "context.script.removeScript";
+				}
+				void executeMethod(span<MiniScript::ScriptVariable>& argumentValues, MiniScript::ScriptVariable& returnValue, const MiniScript::ScriptStatement& statement) override {
+					string scriptId;
+					if (MiniScript::getStringValue(argumentValues, 0, scriptId) == false) {
+						Console::println(getMethodName() + "(): " + miniScript->getStatementInformation(statement) + ": argument mismatch: expected arguments: " + miniScript->getArgumentInformation(getMethodName()));
+						miniScript->startErrorScript();
+					} else {
+						miniScript->getContext()->removeScript(scriptId);
+					}
+				}
+			};
+			miniScript->registerMethod(new ScriptMethodContextScriptRemoveScript(miniScript));
 		}
 	}
 }
