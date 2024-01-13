4ecfbe3533b34108fdc1635e57baeae112ac0539
Andreas Drewke <andreas@drewke.net>
2024-01-13 07:56:05 +0100

    tests: have some moreeeeeee tests

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-./bin/miniscript/tools/miniscript resources/miniscript/tests/class-test.tscript
+./bin/miniscript/tools/miniscript resources/miniscript/tests/advanced-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/application-test.tscript
+./bin/miniscript/tools/miniscript resources/miniscript/tests/base-test.tscript
+./bin/miniscript/tools/miniscript resources/miniscript/tests/class-test.tscript
+./bin/miniscript/tools/miniscript resources/miniscript/tests/constants-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/context-test.tscript
-./bin/miniscript/tools/miniscript resources/miniscript/tests/filesystem-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/cryptography-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/emit-test.tscript
-./bin/miniscript/tools/miniscript resources/miniscript/tests/advanced-test.tscript
-./bin/miniscript/tools/miniscript resources/miniscript/tests/base-test.tscript
-./bin/miniscript/tools/miniscript resources/miniscript/tests/constants-test.tscript
-./bin/miniscript/tools/miniscript resources/miniscript/tests/context-test-2.tscript
-./bin/miniscript/tools/miniscript resources/miniscript/tests/lamdafunctions-test.tscript
-./bin/miniscript/tools/miniscript resources/miniscript/tests/utf8-test.tscript
+./bin/miniscript/tools/miniscript resources/miniscript/tests/filesystem-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/functions-test.tscript
+./bin/miniscript/tools/miniscript resources/miniscript/tests/lamdafunctions-test.tscript
+./bin/miniscript/tools/miniscript resources/miniscript/tests/loop-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/network-test.tscript
+./bin/miniscript/tools/miniscript resources/miniscript/tests/preprocessor-test.tscript
+./bin/miniscript/tools/miniscript resources/miniscript/tests/utf8-test.tscript
+
