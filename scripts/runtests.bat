b4c74486118c85e3f124d5d50796787645460829
Andreas Drewke <andreas@drewke.net>
2024-01-16 11:46:59 +0100

    Arrrrrrrrr. More automated tests

---
+++ scripts/runtests.bat
@@ -0,0 +1,19 @@
+chcp 65001
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\advanced-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\application-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\base-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\class-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\console-test.tscript < scripts\console-test.input
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\constants-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\context-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\cryptography-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\emit-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\filesystem-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\functions-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\lamdafunctions-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\loop-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\network-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\pipe-test.tscript < scripts\pipe-test.input
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\preprocessor-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\switch-test.tscript
+.\bin\miniscript\tools\miniscript resources\miniscript\tests\utf8-test.tscript
--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -3,6 +3,7 @@
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/application-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/base-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/class-test.tscript
+./bin/miniscript/tools/miniscript resources/miniscript/tests/console-test.tscript < ./scripts/console-test.input
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/constants-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/context-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/cryptography-test.tscript
@@ -12,7 +13,7 @@
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/lamdafunctions-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/loop-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/network-test.tscript
+./bin/miniscript/tools/miniscript resources/miniscript/tests/pipe-test.tscript < ./scripts/pipe-test.input
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/preprocessor-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/switch-test.tscript
 ./bin/miniscript/tools/miniscript resources/miniscript/tests/utf8-test.tscript
-
