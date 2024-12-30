38feac79cea056ac907ed757d4ad4ca0db8e561c
Andreas Drewke <andreas@drewke.net>
2024-12-30 18:55:43 +0100

    optionalevents: initial checkin / 3

--- src/minitscript/minitscript/ApplicationMethods.cpp
+++ src/minitscript/minitscript/ApplicationMethods.cpp
@@ -216,6 +216,9 @@ void ApplicationMethods::registerMethods(MinitScript* minitScript) {
 					if ((arguments.size() == 0 || arguments.size() == 1) &&
 						MinitScript::getIntegerValue(arguments, 0, exitCode, true) == true) {
 						minitScript->getContext()->setExitCode(static_cast<int>(exitCode));
+						if (minitScript->isFunctionRunning() == true) {
+							// implement me!
+						}
 						minitScript->stopScriptExecution();
 						minitScript->stopRunning();
 					} else {
